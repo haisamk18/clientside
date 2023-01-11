f2536de6b9a95bcd2a32728cc030bad3eb968706
haisamk18 <khurshidhaisam@gmail.com>
2023-01-11 22:55:46 +0530

    n

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+
+import {Nav, Navbar, Container, NavDropdown, NavLink} from 'react-bootstrap'; 
+import './Navbar.css';
+
+
+function Navbarm() {
+  return (
+    <Navbar className="conmain" expand="lg">
+      <Container className='con' >
+        <Navbar.Brand href="#home" ><span className='brand'>News Wallah</span></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" id='navlinkscon'>
+            <Nav.Link className='Navlinks'  href="#news1"><span className='linktext'>News 1</span></Nav.Link>
+            <Nav.Link className='Navlinks' href="#news2"><span className='linktext'>News 2</span></Nav.Link>
+            <Nav.Link className='Navlinks' href="#news3"><span className='linktext'>News 3</span></Nav.Link>
+            <Nav.Link className='Navlinks' href="#news4"><span className='linktext'>News 4</span></Nav.Link>
+            <Nav.Link className='Navlinks' href="#news5"><span className='linktext'>News 5</span></Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navbarm;
---
+++ src/Components/News.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Newsdata from './Newsdata';
+import Navbarm from './Navbar';
+import './News.css'
+
+
+const News = () => {
+    const [value,setValue]=useState('')
+    const success=false;
+    console.log(value);
+
+    const Searchm =() => {
+        if (Newsdata[0].news.includes(value)){
+            alert("Yes");
+        }
+        if (Newsdata[1].news.includes(value)){
+            alert("Yes");
+        }
+        else {
+            alert("no")
+        }
+        
+    }
+
+
+    
+  return (
+    <div style={{backgroundImage:`url(${process.env.PUBLIC_URL}/assets/ysupport.png)`}}>
+        <Navbarm/>
+        <div style={{display:"flex",justifyContent:"center",gap:"20px"}}>
+          <input type='text' className='inp' onChange={(e)=> setValue(e.target.value)}/>
+          <button className='btn' onClick={Searchm} ><span style={{color:"white",fontWeight:'500'}}>Search</span></button>
+        </div>
+        
+       
+        
+        {Newsdata.filter((e)=> {
+            if (value==''){
+                return e
+            }
+            else if (e.news.includes(value) || e.title.includes(value)) {
+                return (<div>{e.title}</div>)
+            }
+            
+        }).map((e)=> {
+            return (<div id={e.id} className='maindiv'>
+
+                <h3 className='title'>{e.title}</h3>
+                {e.img &&  <div className='image' style={{width:"100%",height:"400px",backgroundImage:`url(${process.env.PUBLIC_URL}/assets/${e.img})`}}></div>}
+                
+                <p className='textnews'>{e.news}</p>
+                
+            </div>)
+        })}
+        <div className='sorry'>Sorry, No match found</div>
+    </div>
+  )
+}
+
+export default News
